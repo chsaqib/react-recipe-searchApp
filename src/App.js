e10c2dbce92d97a69853b2d84a15a5b992c5081c
Saqib Nazir <saqib_9090@yahoo.com>
2019-10-13 20:07:50 +0200

    filtering recipies....

--- src/App.js
+++ src/App.js
@@ -7,22 +7,40 @@ const App = () => {
   const APP_KEY = '85d083ac9f36f63d7b898a165812bed4';
 
   const [recipies, setRecipies] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('chicken');
 
   useEffect(() => {
     getRecipies();
-  }, []);
+  }, [query]);
   const getRecipies = async () => {
     const response = await fetch(
-      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
     );
     const data = await response.json();
     setRecipies(data.hits);
     console.log(data.hits);
   };
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  };
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+  };
+
   return (
     <div className="App">
-      <form className="search-form">
-        <input className="search-bar" type="text"></input>
+      <form onSubmit={getSearch} className="search-form">
+        <input
+          className="search-bar"
+          type="text"
+          value={search}
+          onChange={updateSearch}
+        ></input>
         <button className="search-button" type="submit">
           Search
         </button>
