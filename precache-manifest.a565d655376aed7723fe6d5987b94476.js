8007de6d5888fa5192ba76b7e28cddd20232507c
Saqib Nazir <saqib_9090@yahoo.com>
2019-10-14 17:26:35 +0200

    Updates

---
+++ precache-manifest.a565d655376aed7723fe6d5987b94476.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7b3ef5bf0cffcd0d4cefa558f6f1363",
+    "url": "/react-recipe-searchApp/index.html"
+  },
+  {
+    "revision": "eac9ede074f103b75531",
+    "url": "/react-recipe-searchApp/static/css/main.b4ba7351.chunk.css"
+  },
+  {
+    "revision": "b9a6367ccf5e7766628d",
+    "url": "/react-recipe-searchApp/static/js/2.f0f4a95f.chunk.js"
+  },
+  {
+    "revision": "eac9ede074f103b75531",
+    "url": "/react-recipe-searchApp/static/js/main.aa3b07d7.chunk.js"
+  },
+  {
+    "revision": "0c80cb3f997bfc8f6d55",
+    "url": "/react-recipe-searchApp/static/js/runtime-main.985263fb.js"
+  }
+]);
